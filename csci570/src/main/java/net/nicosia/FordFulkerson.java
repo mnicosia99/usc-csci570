9bdadd8eeef1e5552c9acb54c610612a9d6cc244
Mike Nicosia <mnicosia99@yahoo.com>
2021-03-19 00:54:26 -0700

    modified input graph

--- csci570/src/main/java/net/nicosia/FordFulkerson.java
+++ csci570/src/main/java/net/nicosia/FordFulkerson.java
@@ -67,7 +67,7 @@ class FordFulkerson {
     public static void main(String[] args) throws java.lang.Exception {
         // Let us create a graph shown in the above example
         int graph[][] = new int[][] {
-            { 0, 16, 13, 0, 0, 0 }, { 0, 0, 12, 14, 0, 0 },
+            { 0, 16, 13, 0, 0, 0 }, { 0, 0, 12, 12, 0, 0 },
             { 0, 4, 0, 0, 14, 0 },  { 0, 0, 9, 0, 0, 20 },
             { 0, 0, 0, 7, 0, 4 },   { 0, 0, 0, 0, 0, 0 }
         };
